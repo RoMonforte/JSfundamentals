b2d4b52da1c65fc673e459b5cb93be9f648e4f7d
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2021-03-20 00:59:33 -0600

    playing with for

--- clase1.js
+++ clase1.js
@@ -1,6 +1,29 @@
-var name = 'RmC';
-var last_name = 'Mf';
-var age = 26;
+var rmc = {
+    name: 'Rmc',
+    last_name: 'Mft',
+    age: 28,
+    weight: 30 
+}
 
-var nameMayus = nombre.toUpperCase();
-var last_nameMinus = last_name.toLowerCase ();
+console.log('Al inicio del a;o ' + rmc.name + ' pesa ' + rmc.weight + ' kg');
+
+const INCREMENT = 0.200;
+const DECREMENT = -0.200;
+
+const weightIncrement = person => person.weight += INCREMENT
+const weightDecrement = person => person.weight -= DECREMENT
+
+
+for (var i = 1; i<= 365; i++) {
+    var random = Math.random()
+    if (random < 0.25) {
+        weightIncrement(rmc)
+    } else if (random < 0.50){
+        weightDecrement(rmc)
+    }
+}
+
+console.log 
+
+
+console.log('Al final del a;o ' + rmc.name + ' pesa ' + rmc.weight.toFixed(2) + ' kg');
